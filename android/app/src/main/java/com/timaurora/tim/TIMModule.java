1b11f20b0c0d4578fc738d4ace98a8302a11fb9a
YeomanYe <ming13821007683@gmail.com>
2018-07-21 11:30:13 +0800

    fix tim compile bug

--- android/app/src/main/java/com/timaurora/tim/TIMModule.java
+++ android/app/src/main/java/com/timaurora/tim/TIMModule.java
@@ -1,16 +1,18 @@
 package com.timaurora.tim;
 
 import android.os.Environment;
+import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.tencent.imsdk.TIMLogLevel;
-import com.tencent.imsdk.TIMManager;
-import com.tencent.imsdk.TIMSdkConfig;
+import com.tencent.TIMCallBack;
+import com.tencent.TIMLogLevel;
+import com.tencent.TIMManager;
 
 
 public class TIMModule extends ReactContextBaseJavaModule {
+    private static final String TAG = "TIMModule";
     private ReactApplicationContext rContext;
     public TIMModule(ReactApplicationContext reactContext){
         super(reactContext);
@@ -22,13 +24,28 @@ public class TIMModule extends ReactContextBaseJavaModule {
     }
     @ReactMethod
     public void initSDK(){
+        Log.d(TAG,"initSDK");
         //初始化 SDK 基本配置
-        TIMSdkConfig config = new TIMSdkConfig(Constant.SDK_APPID)
-                .enableCrashReport(false)
-                .enableLogPrint(true)
-                .setLogLevel(TIMLogLevel.DEBUG)
-                .setLogPath(Environment.getExternalStorageDirectory().getPath() + "/tim_log/");
+        TIMManager tim = TIMManager.getInstance();
+        tim.setLogLevel(TIMLogLevel.DEBUG);
+        tim.disableCrashReport();
+        tim.initLogSettings(true,Environment.getExternalStorageDirectory().getPath()+"/tim_log/");
+
         //初始化 SDK
-        TIMManager.getInstance().init(this.rContext, config);
+        tim.init(this.rContext);
+    }
+    @ReactMethod
+    public void login(String identifier,String userSig){
+        TIMManager.getInstance().login(identifier, userSig, new TIMCallBack() {
+            @Override
+            public void onError(int code, String desc) {
+                Log.d(TAG, "login failed. code: " + code + " errmsg: " + desc);
+            }
+
+            @Override
+            public void onSuccess() {
+                Log.d(TAG, "login succ");
+            }
+        });
     }
 }
